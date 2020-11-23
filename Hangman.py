d9424815f4eb7e7dba13d421a378e45cd4588e99
jhoang10 <74330087+jhoang10@users.noreply.github.com>
2020-11-23 13:02:41 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,2 +1,80 @@
 import random
 from WordList import wordList
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+  
+  def display_hangman(tries): #we could use this or just make the code display how many tries the user has left without the stickman
+        stages = [ """  
+                        --------
+                        |    |
+                        |    o
+                        |   \\|/
+                        |    |
+                        |   / \\
+                        -
+                    """
+                        ,
+                    """
+                        
+                        --------
+                        |     |
+                        |     o
+                        |    \\|/
+                        |     |
+                        |    /
+                        -
+                    """
+                        ,
+                    """
+                        --------
+                        |     |
+                        |     o
+                        |    \\|/
+                        |     |
+                        |
+                        -
+                    """
+                        ,
+                    """
+                        --------
+                        |     |
+                        |     o
+                        |    \\|
+                        |     |
+                        |
+                        -
+                    """
+                        ,
+                    """
+                        --------
+                        |     |
+                        |     o
+                        |     |
+                        |     |
+                        |
+                        -
+                    """
+                        ,
+                    """
+                        --------
+                        |     |
+                        |     o
+                        |
+                        |
+                        |
+                        -
+                    """
+                        ,
+                    """
+                        --------
+                        |     |
+                        |
+                        |
+                        |
+                        |
+                        -
+                    """
+    ]
+    return stages[tries]

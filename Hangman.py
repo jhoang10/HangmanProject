d929ae106ace32e44efe3279232bb70b1e2041e3
jhoang10 <74330087+jhoang10@users.noreply.github.com>
2020-11-25 12:10:07 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -9,6 +9,22 @@ WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman")
 
+
+#buttons 
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+A = 65
+for i in range(26):
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x,y, chr(A + i)])
+
+#fonts
+LETTER_FONT = pygame.font.SysFont('arial', 40)
+
 #hangman pictures used
 images = []
 #loop cycles through the images for each point in hangman based on pic name
@@ -18,12 +34,29 @@ for i in range(7):
 
 #stage in where the hangman is
 hangmanLife = 0
+words = []
+words = random.choice(wordlist)
+guessed = []
+
+#colors
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+#draw buttons
+def draw():
+    win.fill(WHITE)
+    
+    for letter in letters:
+        x,y, ltr = letter
+        pygame.draw.circle(win, BLACK, (x,y), RADIUS, 3)
+        text = LETTER_FONT.render(ltr, 1, BLACK)
+        win.blit(text, (x - text.get_width()/2,y - text.get_height()/2))
 
 
 FPS = 60
 clock = pygame.time.Clock()
 run = True
-
+    
 
 while run:
     clock.tick(FPS)
